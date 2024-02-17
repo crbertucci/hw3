ff31b655ba6234939c3ddf9faed3a22e667e1e24
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-02-17 01:32:22 +0000

    2161932

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,7 @@
 class PlacesController < ApplicationController
 
   def index 
-    render :inline => "<h1> Places I've Been</h1>"
+  #  render :inline => "<h1> Places I've Been</h1>"
+  render :template => "places/index"
   end
 end
