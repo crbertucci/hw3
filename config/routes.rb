40b792cdcfa677b7edfc6a7840073072107b5efa
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-02-17 04:14:49 +0000

    2162214

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/places", :controller => "places", :action => "index"
+  resources "places"
 
-
-  get "/entries", :controller => "entries", :action => "index"
+resources "entries"
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
